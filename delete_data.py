ba25b42b57fb7f9b55cd2d97a1b9726361e4a330
Matan-Abir <matan.abir@gmail.com>
2026-04-13 23:13:51 +0300

    Updated delete_data to use utilities.show_event_list()

--- delete_data.py
+++ delete_data.py
@@ -1,16 +1,14 @@
+import utilitis
 def delete_event(events_data):
     if len(events_data) == 0:
         print("No events to delete.")
     else:
         print("Summary of all events: \n")
-        i = 0
-        for event in events_data:
-            i += 1
-            print(f"[{i}] - {event['event']}, held on {event['date']} at {event['venue']}")
+        utilitis.show_events_list(events_data)
         try:
             user_choice = int(input("\Enter the number of event to delete, or -1 to cancel: "))
             if user_choice != -1:
                 del events_data[user_choice-1]
                 print("Event deleted!")
         except:
-            print(f"Error! Input has to be a whole number between 1 and {i}.")
+            print(f"Error! Input has to be a whole number between 1 and {len(events_data)}.")
